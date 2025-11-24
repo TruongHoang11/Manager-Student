571c06ea64a94812ca06104760670e9932d2cc95
TruongHoang11 <truongconghuyhoang1407@gmail.com>
2025-11-24 19:16:29 +0700

    Delete JavaSpringBoot Full SKill directory

--- JavaSpringBoot Full SKill/hoangto-sample-code/Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM openjdk:17
-
-ARG FILE_JAR=target/*.jar
-
-ADD ${FILE_JAR} api-service.jar
-
-ENTRYPOINT ["java", "-jar", "api-service.jar"]
-
-EXPOSE 80
--- JavaSpringBoot Full SKill/hoangto-sample-code/src/main/java/vn/hoangtojava/dto/response/ResponseError.java
+++
@@ -1,7 +0,0 @@
-package vn.hoangtojava.dto.response;
-
-public class ResponseError extends ResponseData{
-    public ResponseError(int status, String message) {
-        super(status, message);
-    }
-}
